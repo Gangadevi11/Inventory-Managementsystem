0bc7c4fd89811d69571086e2a97de16f23a1ce0f
Gangadevi11 <113599320+Gangadevi11@users.noreply.github.com>
2024-09-21 09:31:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import InventoryRouter from './inventoryrouter';
+import AppRouter from "./approuter";
+
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <AppRouter/>
+    </div>
+  );  
+}
+
+export default App;
